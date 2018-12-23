f003ed54f3b2bad2d7e6eeaf42c112ddd732ba89
Mahmoud Shaheen <mahmoud_shaheen@live.com>
2018-12-23 04:39:38 +0200

    Unit Tests

---
+++ CalcTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2e4a4e0-8007-4420-b78a-7e30a3598a76")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalcTest/UnitTest1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCrossCalculator;
+namespace CalcTest
+{
+    [TestClass]
+    public class CalcTest
+    {
+        Calc Calc = new Calc();
+
+        [TestMethod]
+        public void TestDotClickHandler()
+        {
+            Assert.AreEqual("0.", Calc.DotClickHandler("0"));
+            Assert.AreEqual("10.", Calc.DotClickHandler("10"));
+            Assert.AreEqual("0.1", Calc.DotClickHandler("0.1"));
+        }
+
+        [TestMethod]
+        public void TestSignClickHandler()
+        {
+            Assert.AreEqual("0", Calc.SignClickHandler("0"));
+            Assert.AreEqual("-10", Calc.SignClickHandler("10"));
+            Assert.AreEqual("0.1", Calc.SignClickHandler("-0.1"));
+        }
+
+        [TestMethod]
+        public void TestNumberClickHandler()
+        {
+            Assert.AreEqual("0", Calc.NumberClickHandler("0", "0"));
+            Assert.AreEqual("-10", Calc.NumberClickHandler("-1", "0"));
+            Assert.AreEqual("10", Calc.NumberClickHandler("1", "0"));
+            Assert.AreEqual("0.10", Calc.NumberClickHandler("0.1", "0"));
+        }
+
+        [TestMethod]
+        public void TestEqual()
+        {
+            string screenText = "0";
+            screenText = Calc.OperationClickHandler("0", "=");
+            Assert.AreEqual("0", screenText);
+            screenText = Calc.OperationClickHandler("11", "+");
+            screenText = Calc.OperationClickHandler("11", "=");
+            screenText = Calc.OperationClickHandler("22", "=");
+            Assert.AreEqual("22", screenText);
+            screenText = Calc.OperationClickHandler("22", "=");
+            Assert.AreEqual("22", screenText);
+            screenText = Calc.OperationClickHandler("11", "+");
+            screenText = Calc.OperationClickHandler("11", "=");
+            screenText = Calc.OperationClickHandler("22", "=");
+            Assert.AreEqual("22", screenText);
+        }
+
+        [TestMethod]
+        public void TestOperationAdd()
+        {
+            string screenText = "";
+            screenText = Calc.OperationClickHandler("11", "+");
+            screenText = Calc.OperationClickHandler("11", "=");
+            Assert.AreEqual("22", screenText);
+        }
+
+        [TestMethod]
+        public void TestOperationSub()
+        {
+            string screenText = "";
+            screenText = Calc.OperationClickHandler("11", "-");
+            screenText = Calc.OperationClickHandler("1", "=");
+            Assert.AreEqual("10", screenText);
+        }
+
+        [TestMethod]
+        public void TestOperationMulti()
+        {
+            string screenText = "";
+            screenText = Calc.OperationClickHandler("11", "*");
+            screenText = Calc.OperationClickHandler("5", "=");
+            Assert.AreEqual("55", screenText);
+        }
+
+        [TestMethod]
+        public void TestOperationDiv()
+        {
+            string screenText = "";
+            screenText = Calc.OperationClickHandler("22", "/");
+            screenText = Calc.OperationClickHandler("11", "=");
+            Assert.AreEqual("2", screenText);
+        }
+
+        [TestMethod]
+        public void TestAllOperations()
+        {
+            string screenText = "";
+            screenText = Calc.OperationClickHandler("11", "+");
+            Assert.AreEqual("0", screenText);
+            screenText = Calc.OperationClickHandler("11", "=");
+            Assert.AreEqual("22", screenText);
+
+            screenText = Calc.OperationClickHandler(screenText, "-");
+            Assert.AreEqual("0", screenText);
+            screenText = Calc.OperationClickHandler("11", "=");
+            Assert.AreEqual("11", screenText);
+
+            screenText = Calc.OperationClickHandler(screenText, "*");
+            Assert.AreEqual("0", screenText);
+            screenText = Calc.OperationClickHandler("5", "=");
+            Assert.AreEqual("55", screenText);
+
+            screenText = Calc.OperationClickHandler(screenText, "/");
+            Assert.AreEqual("0", screenText);
+            screenText = Calc.OperationClickHandler("5", "=");
+            Assert.AreEqual("11", screenText);
+        }
+
+        [TestMethod]
+        public void TestClearClickHandler()
+        {
+            string screenText = "";
+            screenText = Calc.OperationClickHandler("22", "/");
+            screenText = Calc.OperationClickHandler("11", "=");
+            Assert.AreEqual("2", screenText);
+            screenText = Calc.ClearClickHandler();
+            Assert.AreEqual("0", screenText);
+            screenText = Calc.OperationClickHandler("22", "/");
+            screenText = Calc.OperationClickHandler("11", "=");
+            Assert.AreEqual("2", screenText);
+        }
+    }
+}
